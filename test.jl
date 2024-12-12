499e68de3d5059747e840162d24934d0e901b4f2
hamster <2665097234@qq.com>
2024-12-13 00:12:08 +0800

    241212

--- test.jl
+++ test.jl
@@ -1,4 +1,7 @@
-txt = open("data/runoob-file.txt","a") do file
-    read(file, String)
-end
-println(txt)
+using DataFrames
+da0 = DataFrame(
+    name=["a", "b", "c", "d"], 
+    age=[33, 42, missing, 51],
+    sex=["M", "F", "M", "M"])
+da1 = copy(da0)
+ismissing.(da1[!,:age])|>show
